0c2448d9c35c7791c5e9aaa34a054de3545b4cb6
Nathan Staab <nathanstaab@playbackrewards.com>
2024-06-21 21:00:19 -0400

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import AnimatedIntro from "../components/AnimatedIntro";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <AnimatedIntro />
+    </View>
+  );
+}
